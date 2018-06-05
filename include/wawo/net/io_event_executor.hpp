413b60b290a738b328c691c8887dc9464b38cf1e
ycopy <io.cpp@outlook.com>
2018-06-05 15:49:05 +0800

    aaa

---
+++ include/wawo/net/io_event_executor.hpp
@@ -0,0 +1,114 @@
+#ifndef _WAWO_NET_IO_EVENT_EXECUTOR_HPP
+#define _WAWO_NET_IO_EVENT_EXECUTOR_HPP
+
+#include <wawo/core.hpp>
+#include <wawo/smart_ptr.hpp>
+#include <wawo/thread/thread_run_object_abstract.hpp>
+#include <wawo/thread/mutex.hpp>
+
+#include <wawo/task/task.hpp>
+#include <wawo/timer.hpp>
+
+#include <queue>
+
+namespace wawo { namespace net {
+	//@todo impl block until no new task feature
+	using namespace wawo::thread;
+
+	typedef std::queue<WWRP<wawo::task::task_abstract>> TASK_Q;
+	class io_event_executor :
+		public wawo::ref_base,
+		public wawo::net::thread_run_object_abstract
+	{
+		typedef std::function<void()> fn_io_event_task;
+
+		spin_mutex m_tq_mtx;
+		WWSP<TASK_Q> m_tq_standby;
+		WWSP<TASK_Q> m_tq;
+		WWSP<timer_manager> m_tm;
+		std::thread::id m_tid;
+
+		inline void __exec__() {
+			WAWO_ASSERT(m_tq->size() == 0);
+			{
+				lock_guard<spin_mutex> lg(m_tq_mtx);
+				if (m_tq_standby->size() > 0) {
+					std::swap(m_tq, m_tq_standby);
+				}
+			}
+
+			while (m_tq->size()) {
+				WWRP<wawo::task::task_abstract>& t = m_tq->front();
+				WAWO_ASSERT(t != NULL);
+				t->run();
+				m_tq->pop();
+			}
+		}
+
+	public:
+		io_event_executor() {}
+		virtual ~io_event_executor() {
+			WAWO_ASSERT(m_tq->empty());
+			WAWO_ASSERT(m_tq_standby->empty());
+		}
+
+		
+		inline void schedule(WWRP<wawo::task::task_abstract> const& t) {
+			WAWO_ASSERT(t != NULL);
+			lock_guard<spin_mutex> lg(m_tq_mtx);
+			m_tq_standby->push(t);
+		}
+		
+
+		//template <class _Task>
+		//inline void schedule(_Task&& task)
+		//{}
+
+		//template <class _Task,
+		//	class = typename std::enable_if<std::is_base_of<wawo::task::task_abstract,typename _Task::ELEMENT_TYPE>::value>::type>
+		//inline void schedule(_Task&& t) {
+		//	WAWO_ASSERT(t != NULL);
+		//	lock_guard<spin_mutex> lg(m_tq_mtx);
+		//	m_tq_standby->push(t);
+		//}
+
+		inline void schedule(fn_io_event_task&& f) {
+			WWRP<wawo::task::lambda_task> _t = wawo::make_ref<wawo::task::lambda_task>( std::forward<fn_io_event_task>(f));
+			lock_guard<spin_mutex> lg(m_tq_mtx);
+			m_tq_standby->push(_t);
+		}
+
+		inline bool in_event_loop() const {
+			return std::this_thread::get_id() == m_tid;
+		}
+
+		inline void start_timer(WWRP<wawo::timer> const& t) {
+			m_tm->start(t);
+		}
+		inline void stop_timer(WWRP<wawo::timer> const& t) {
+			m_tm->stop(t);
+		}
+
+		void on_start() {
+			m_tid = std::this_thread::get_id();
+			m_tq_standby = wawo::make_shared<TASK_Q>();
+			m_tq = wawo::make_shared<TASK_Q>();
+
+			m_tm = wawo::make_shared<timer_manager>(false);
+		}
+
+		void on_stop() {
+			WAWO_ASSERT(m_tq->empty());
+			WAWO_ASSERT(m_tq_standby->empty());
+
+			m_tm = NULL;
+		}
+
+		void run() {
+			__exec__();
+		}
+	};
+
+
+}}
+#endif
---
+++ include/wawo/net/io_event_loop.hpp
@@ -0,0 +1,125 @@
+#ifndef _WAWO_NET_IO_EVENT_LOOP_HPP
+#define _WAWO_NET_IO_EVENT_LOOP_HPP
+
+#include <wawo/net/io_event_executor.hpp>
+#include <wawo/net/socket_observer.hpp>
+#include <wawo/net/wcp.hpp>
+
+namespace wawo { namespace net {
+	class io_event_loop :
+		public io_event_executor
+	{
+		WWRP<socket_observer> m_observer;
+		u8_t m_observer_type;
+	public:
+		io_event_loop(u8_t t = get_os_default_poll_type()) :
+			m_observer_type(t)
+		{}
+		~io_event_loop() {}
+
+		u8_t get_type() const { return m_observer_type; }
+
+		void on_start() {
+			io_event_executor::on_start();
+
+			WAWO_ASSERT(m_observer == NULL);
+			m_observer = wawo::make_ref<socket_observer>(m_observer_type);
+			WAWO_ALLOC_CHECK(m_observer, sizeof(socket_observer));
+			m_observer->init();
+		}
+
+		void on_stop() {
+			io_event_executor::on_stop();
+
+			WAWO_ASSERT(m_observer != NULL);
+			m_observer->deinit();
+			m_observer = NULL;
+		}
+
+		void run() {
+			io_event_executor::run();
+			m_observer->update();
+			io_event_executor::run();
+			wawo::this_thread::nsleep(observer_checker_interval);
+		}
+
+		void watch(u8_t const& flag, int const& fd, WWRP<ref_base> const& cookie, fn_io_event const& fn, fn_io_event_error const& err) {
+			m_observer->watch(flag, fd, cookie, fn, err);
+		}
+		void unwatch(u8_t const& flag, int const& fd) {
+			m_observer->unwatch(flag, fd);
+		}
+	};
+
+	typedef std::vector<WWRP<io_event_loop>> io_event_loop_vector;
+	class io_event_loop_group :
+		public wawo::singleton<io_event_loop_group>
+	{
+	private:
+		std::atomic<int> m_curr_sys;
+		std::atomic<int> m_curr_wpoll;
+		io_event_loop_vector m_event_loops;
+		io_event_loop_vector m_wpoll_event_loops;
+	public:
+		io_event_loop_group()
+			:m_curr_sys(0), m_curr_wpoll(0)
+		{}
+		~io_event_loop_group() {}
+		void init(int wpoller_count = 1) {
+			int i = std::thread::hardware_concurrency();
+			int sys_i = i - wpoller_count;
+			if (sys_i <= 0) {
+				sys_i = 1;
+			}
+
+			while (sys_i-- > 0) {
+				WWRP<io_event_loop> o = wawo::make_ref<io_event_loop>();
+				int rt = o->start();
+				WAWO_ASSERT(rt == wawo::OK);
+				m_event_loops.push_back(o);
+			}
+
+			wpoller_count = WAWO_MIN(wpoller_count, 4);
+			wpoller_count = WAWO_MAX(wpoller_count, 1);
+
+			if (wpoller_count > 0) {
+				wcp::instance()->start();
+			}
+
+			while (wpoller_count-- > 0) {
+				WWRP<io_event_loop> o = wawo::make_ref<io_event_loop>(T_WPOLL);
+				int rt = o->start();
+				WAWO_ASSERT(rt == wawo::OK);
+				m_wpoll_event_loops.push_back(o);
+			}
+		}
+		WWRP<io_event_loop> next(bool const& return_wpoller = false) {
+			if (return_wpoller) {
+				int i = m_curr_wpoll.load() % m_wpoll_event_loops.size();
+				wawo::atomic_increment(&m_curr_wpoll);
+				return m_wpoll_event_loops[i% m_wpoll_event_loops.size()];
+			}
+			else {
+				int i = m_curr_sys.load() % m_event_loops.size();
+				wawo::atomic_increment(&m_curr_sys);
+				return m_event_loops[i% m_event_loops.size()];
+			}
+		}
+		void deinit() {
+			if (m_wpoll_event_loops.size()) {
+				wcp::instance()->stop();
+				std::for_each(m_wpoll_event_loops.begin(), m_wpoll_event_loops.end(), [](WWRP<io_event_loop> const& o) {
+					o->stop();
+				});
+				m_wpoll_event_loops.clear();
+			}
+
+			std::for_each(m_event_loops.begin(), m_event_loops.end(), [](WWRP<io_event_loop> const& o) {
+				o->stop();
+			});
+			m_event_loops.clear();
+		}
+	};
+
+}}
+#endif
