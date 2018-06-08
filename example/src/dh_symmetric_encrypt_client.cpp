ac3da805224340a3acad4acde0d5d838b28d11e6
ycopy <io.cpp@outlook.com>
2018-06-08 21:59:09 +0800

    fix example

--- example/src/dh_symmetric_encrypt_client.cpp
+++ example/src/dh_symmetric_encrypt_client.cpp
@@ -18,37 +18,30 @@ int main(int argc, char** argv) {
 
 	wawo::app app;
 
-	{
-		WWRP<wawo::net::socket> so = wawo::make_ref<wawo::net::socket>(wawo::net::F_AF_INET, wawo::net::T_STREAM, wawo::net::P_TCP);
-		int rt = so->open();
+	WWRP<wawo::net::socket> so = wawo::make_ref<wawo::net::socket>(wawo::net::F_AF_INET, wawo::net::T_STREAM, wawo::net::P_TCP);
+	wawo::net::address addr("127.0.0.1", 22311);
 
-		if (rt != wawo::OK) {
-			so->close();
-			return rt;
-		}
-
+	WWRP<wawo::net::channel_future> f_connect = so->dial(addr, [](WWRP<wawo::net::channel> const& ch) {
 		WWRP<wawo::net::channel_handler_abstract> hlenh = wawo::make_ref<wawo::net::handler::hlen>();
-		so->pipeline()->add_last(hlenh);
+		ch->pipeline()->add_last(hlenh);
 
 		WWRP<wawo::net::channel_handler_abstract> dhh = wawo::make_ref<wawo::net::handler::dh_symmetric_encrypt>();
-		so->pipeline()->add_last(dhh);
+		ch->pipeline()->add_last(dhh);
 
 		WWRP<wawo::net::channel_handler_abstract> dumpjn = wawo::make_ref<wawo::net::handler::dump_in_len>();
-		so->pipeline()->add_last(dumpjn);
+		ch->pipeline()->add_last(dumpjn);
 
 		WWRP<wawo::net::channel_handler_abstract> echoh = wawo::make_ref<wawo::net::handler::echo>();
-		so->pipeline()->add_last(echoh);
+		ch->pipeline()->add_last(echoh);
 
 		WWRP<wawo::net::channel_handler_abstract> sendh = wawo::make_ref<send_hello>();
-		so->pipeline()->add_last(sendh);
+		ch->pipeline()->add_last(sendh);
+	});
 
-		wawo::net::address addr("127.0.0.1", 22311);
-		WWRP < wawo::net::channel_future> f_connect = so->async_connect(addr);
-
-		WAWO_ASSERT(f_connect->get() == wawo::OK );
-		so->ch_close_future()->wait();
-	}
-
+	WAWO_ASSERT(f_connect->get() == wawo::OK );
 	app.run_for();
+
+	so->ch_close();
+	so->ch_close_future()->wait();
 	return wawo::OK;
 }
--- example/src/dh_symmetric_encrypt_server.cpp
+++ example/src/dh_symmetric_encrypt_server.cpp
@@ -2,12 +2,14 @@
 
 #include <wawo.h>
 
-class listen_handler :
-	public wawo::net::channel_acceptor_handler_abstract
-{
-public:
+int main(int argc, char** argv) {
 
-	void accepted(WWRP<wawo::net::channel_handler_context> const& ctx, WWRP<wawo::net::channel> const& ch) {
+	wawo::app app;
+
+	WWRP<wawo::net::socket> so = wawo::make_ref<wawo::net::socket>(wawo::net::F_AF_INET, wawo::net::T_STREAM, wawo::net::P_TCP);
+	wawo::net::address addr("0.0.0.0", 22311);
+
+	WWRP<wawo::net::channel_future> f_listen = so->listen_on(addr, [](WWRP<wawo::net::channel> const& ch) {
 		WWRP<wawo::net::channel_handler_abstract> hlen_handler = wawo::make_ref<wawo::net::handler::hlen>();
 		ch->pipeline()->add_last(hlen_handler);
 
@@ -19,37 +21,10 @@ public:
 
 		WWRP<wawo::net::channel_handler_abstract> echo = wawo::make_ref<wawo::net::handler::echo>();
 		ch->pipeline()->add_last(echo);
-	}
+	});
 
-};
-
-
-int main(int argc, char** argv) {
-
-	wawo::app app;
-
-	WWRP<wawo::net::socket> so = wawo::make_ref<wawo::net::socket>(wawo::net::F_AF_INET, wawo::net::T_STREAM, wawo::net::P_TCP);
-	int rt = so->open();
-
+	int rt = f_listen->get();
 	if (rt != wawo::OK) {
-		so->close();
-		return rt;
-	}
-
-	wawo::net::address addr("0.0.0.0", 22311);
-	WWRP<wawo::net::channel_future> f_bind = so->async_bind(addr);
-
-	if (f_bind->get() != wawo::OK) {
-		so->ch_close();
-		return rt;
-	}
-
-	WWRP<wawo::net::channel_handler_abstract> h = wawo::make_ref<listen_handler>();
-	so->pipeline()->add_last(h);
-
-	WWRP<wawo::net::channel_future> f_listen = so->async_listen();
-
-	if (f_listen->get() != wawo::OK) {
 		so->ch_close();
 		return rt;
 	}
