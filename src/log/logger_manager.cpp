9ae27ad76d10854221430fdcd30b93ca493ed0d8
ycopy <io.cpp@outlook.com>
2018-07-31 11:27:21 +0800

    [fix]modify check logic for log formart according to https://linux.die.net/man/3/vsnprintf

--- src/log/logger_manager.cpp
+++ src/log/logger_manager.cpp
@@ -54,7 +54,7 @@ namespace wawo { namespace log {
 					++it;
 				}
 			}
-			m_logBufferIdx = 0 ;
+			m_logBufferIdx = 0;
 		}
 
 		m_isInited = true;
@@ -64,17 +64,18 @@ namespace wawo { namespace log {
 		m_loggers.clear();
 	}
 
-	void logger_manager::add_logger( WWRP<logger_abstract> const& logger) {
+	void logger_manager::add_logger(WWRP<logger_abstract> const& logger) {
 		m_loggers.push_back(logger);
 	}
 
-	void logger_manager::remove_logger( WWRP<logger_abstract> const& logger ) {
+	void logger_manager::remove_logger(WWRP<logger_abstract> const& logger) {
 		std::vector< WWRP<logger_abstract> >::iterator it = m_loggers.begin();
 
-		while( it != m_loggers.end() ) {
-			if( (*it) == logger ) {
-				it = m_loggers.erase( it );
-			} else {
+		while (it != m_loggers.end()) {
+			if ((*it) == logger) {
+				it = m_loggers.erase(it);
+			}
+			else {
 				++it;
 			}
 		}
@@ -83,62 +84,62 @@ namespace wawo { namespace log {
 		return m_defaultFormatInterface;
 	}
 
-	#define LOG_BUFFER_SIZE_MAX		(1024*4)
-	#define TRACE_INFO_SIZE 1024
+#define LOG_BUFFER_SIZE_MAX		(1024*4)
+#define TRACE_INFO_SIZE 1024
 
-	void logger_manager::write( log_mask const& mask, char const* const file, int line, char const* const func, ... ) {
+	void logger_manager::write(log_mask const& mask, char const* const file, int line, char const* const func, ...) {
 
 #ifdef _DEBUG
-		char __traceInfo[TRACE_INFO_SIZE] = {0};
-		snprintf(__traceInfo, ((sizeof(__traceInfo)/sizeof(__traceInfo[0])) - 1), "TRACE: %s:[%d] %s", file, line, func );
+		char __traceInfo[TRACE_INFO_SIZE] = { 0 };
+		snprintf(__traceInfo, ((sizeof(__traceInfo) / sizeof(__traceInfo[0])) - 1), "TRACE: %s:[%d] %s", file, line, func);
 #endif
 		const wawo::u64_t tid = wawo::this_thread::get_id();
-		char log_buffer[LOG_BUFFER_SIZE_MAX] = {0};
+		char log_buffer[LOG_BUFFER_SIZE_MAX] = { 0 };
 
 		std::string local_time_str;
 		wawo::time::curr_localtime_str(local_time_str);
 		wawo::size_t idx_tid = 0;
-		int snwrite = snprintf( log_buffer + idx_tid, LOG_BUFFER_SIZE_MAX- idx_tid, "[%s][%c][%llu]", local_time_str.c_str(),__log_mask_char[mask], tid);
+		int snwrite = snprintf(log_buffer + idx_tid, LOG_BUFFER_SIZE_MAX - idx_tid, "[%s][%c][%llu]", local_time_str.c_str(), __log_mask_char[mask], tid);
 		if (snwrite == -1) {
 			WAWO_THROW("snprintf failed for loggerManager::write");
 		}
-		WAWO_ASSERT( (wawo::size_t)snwrite < (LOG_BUFFER_SIZE_MAX - idx_tid));
+		WAWO_ASSERT((wawo::size_t)snwrite < (LOG_BUFFER_SIZE_MAX - idx_tid));
 		idx_tid += snwrite;
 
 		//for log buffer
-		if( !m_isInited )
+		if (!m_isInited)
 		{
 			WWSP<format_interface> formatInterface;
 
 			formatInterface = get_default_format_interface();
 			va_list valist;
 
-			va_start( valist, func );
-			char* fmt = va_arg(valist , char*);
-			int size = formatInterface->format( log_buffer + idx_tid, LOG_BUFFER_SIZE_MAX-idx_tid, fmt, valist );
+			va_start(valist, func);
+			char* fmt = va_arg(valist, char*);
+			int size = formatInterface->format(log_buffer + idx_tid, LOG_BUFFER_SIZE_MAX - idx_tid, fmt, valist);
 			va_end(valist);
 
-			m_log_buffer[m_logBufferIdx] = std::string(log_buffer, size ) ;
+			m_log_buffer[m_logBufferIdx] = std::string(log_buffer, size);
 
 #ifdef _DEBUG
 			m_log_buffer[m_logBufferIdx] += __traceInfo;
 #endif
 			m_log_buffer_mask[m_logBufferIdx] = mask;
-			m_logBufferIdx ++ ;
+			m_logBufferIdx++;
 
 			(void)line;
-			return ;
+			return;
 		}
 
 		WWSP<format_interface> formatInterface;
 		std::vector< WWRP<logger_abstract> >::iterator it = m_loggers.begin();
-		while( it != m_loggers.end() ) {
+		while (it != m_loggers.end()) {
 			if (!(*it)->test_mask(mask)) { ++it; continue; }
-			WAWO_ASSERT( (*it) != NULL );
+			WAWO_ASSERT((*it) != NULL);
 
 			int idx_fmt = idx_tid;
-			formatInterface = (*it)->get_formator() ;
-			if( formatInterface == NULL ) {
+			formatInterface = (*it)->get_formator();
+			if (formatInterface == NULL) {
 				formatInterface = get_default_format_interface();
 			}
 
@@ -146,12 +147,17 @@ namespace wawo { namespace log {
 			va_start(valist, func);
 			char* fmt;
 			fmt = va_arg(valist, char*);
-			int fmtsize = formatInterface->format( log_buffer+idx_fmt, LOG_BUFFER_SIZE_MAX- idx_fmt, fmt, valist );
+			int fmtsize = formatInterface->format(log_buffer + idx_fmt, LOG_BUFFER_SIZE_MAX - idx_fmt, fmt, valist);
 			va_end(valist);
 
-			WAWO_ASSERT(fmtsize != -1 );
-			WAWO_ASSERT(fmtsize < (LOG_BUFFER_SIZE_MAX - idx_fmt));
-			idx_fmt += fmtsize;
+			WAWO_ASSERT(fmtsize != -1);
+			//@refer to: https://linux.die.net/man/3/vsnprintf
+			if (fmtsize >= (LOG_BUFFER_SIZE_MAX-idx_fmt)) {
+				//truncated
+				fmtsize = (LOG_BUFFER_SIZE_MAX - idx_fmt) - 1;
+			} else {
+				idx_fmt += fmtsize;
+			}
 			WAWO_ASSERT(idx_fmt < LOG_BUFFER_SIZE_MAX);
 
 #ifdef _DEBUG
